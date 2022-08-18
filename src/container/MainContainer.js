f83eab9b65dae742c6ddb35f2a8ceabbde30f99e
woohyuk <dngur6344@gmail.com>
2022-08-19 00:12:53 +0900

    헬스장 목록 띄우는 화면 엔드포인트 분리

--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -3,19 +3,8 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as mainActions from "./../store/module/main";
 import GymList from "./../component/gym/GymList";
-import {Link} from 'react-router-dom';
-import './TabStyle.css';
 
 class MainContainer extends Component {
-
-  constructor(props) {
-    super();
-
-    this.state = {
-      menu: 0,
-    };
-  }
-
   initialize = () => {
     const { MainActions } = this.props;
     MainActions.getGymList();
@@ -31,76 +20,21 @@ class MainContainer extends Component {
     MainActions.changeInput({name, value});
   }
 
-  handleClick = (e) => {
-    const { MainActions } = this.props;
-    MainActions.getGymListByName(e);
-    this.render();
-  }
-
-  changeMenu = (menuIndex) =>{
-    this.setState({menu : menuIndex});
-  }
-
   render() {
-    const menuList = {
-      0: <TabList gyms={this.props.gyms}/>,
-      1: <div>asdf</div>,
-    };
-
-    const { gyms, search } = this.props;
+    const { gyms, category } = this.props;
+    debugger;
     return (
       <div>
-        {/* <GymList gyms={gyms} onChangeInput={this.handleChangeInput} category={category}/> */}
-        <input type="text" name="search" onChange={this.handleChangeInput}/>
-        <button onClick={() => this.handleClick(search)}>
-          검색
-        </button>
-        <button onClick={() => this.initialize()}>
-          초기화
-        </button>
-        <div className="wrap">
-          <div className="menuBar">
-            <ul className="tabs">
-              <li className={`${this.state.menu === 0? 'active': ''}`} onClick={() => this.changeMenu(0)}>헬스장</li>
-              <li className={`${this.state.menu === 1? 'active': ''}`} onClick={() => this.changeMenu(1)}>트레이너</li>
-            </ul>
-          </div>
-          <div className="contentArea">
-            {menuList[this.state.menu]}
-          </div>
-        </div>
-          {/* <TabList gyms={gyms}/> */}
+        <GymList gyms={gyms} onChangeInput={this.handleChangeInput} category={category}/>
       </div>
     );
   }
 }
 
-const TabList = ({gyms}) => {
-  return (
-    <div>
-      <ul>
-          {gyms.map((gym,index) => (
-            <li style={{
-              border: '1px solid black',
-              padding: '25px',
-              margin: '15px',
-            }} key={index.toString()}>
-              <Link to="/gym" state={{gym : {gym} }}>
-              <GymList gym={gym}  key={index}/>
-              </Link>
-            </li>
-          ))}
-          </ul>
-    </div>
-  );
-};
-
-
-
 export default connect(
   (state) => ({
     gyms: state.main.get("gyms"),
-    search: state.main.get("search")
+    category: state.main.get("category")
   }),
   (dispatch) => ({
     MainActions: bindActionCreators(mainActions, dispatch),
--- src/page/index.js
+++ src/page/index.js
@@ -1,2 +1,3 @@
 export { default as MainPage } from './MainPage';
+export { default as GymListPage} from './GymListPage';
 export { default as GymDetailsPage} from './GymDetailsPage';
